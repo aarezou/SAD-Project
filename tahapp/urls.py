8f386204376893c2c4223cc3b24c882565f226dc
iliad <ili1dr1m@gmail.com>
2018-02-03 12:54:00 +0330

    changed tabs in urls and loged in

--- tahapp/urls.py
+++ tahapp/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('needful/', views.needful, name='needful'),
+	path('', views.index, name='index'),
+	path('needful/', views.needful, name='needful'),
 	path('helper/', views.helper, name='helper'),
-    path('login/', views.login, name='login'),
+	path('login/', views.login, name='login'),
 	path('logout', views.logout, name='needCare'),
-    path('register/', views.register, name='register'),
+	path('register/', views.register, name='register'),
 	path('submitLetter', views.submitLetter, name='letter'),
 	path('helperChange', views.submitHelperChange, name='helperChange'),
 	path('needCare', views.submitNeedCare, name='needCare'),
